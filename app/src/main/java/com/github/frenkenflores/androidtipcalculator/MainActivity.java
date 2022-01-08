b9c088cd20f87ece1d1222703a636af177560d65
FrenkenFlores <saifualdin.evloev@gmail.com>
2022-01-09 01:47:20 +0300

    4* coded the bussines logic

--- app/src/main/java/com/github/frenkenflores/androidtipcalculator/MainActivity.java
+++ app/src/main/java/com/github/frenkenflores/androidtipcalculator/MainActivity.java
@@ -3,12 +3,16 @@ package com.github.frenkenflores.androidtipcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
     private TextView tvBase;
     private TextView tvSeekbar;
     private TextView tvTip;
@@ -32,23 +36,53 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         etBaseAmount = (EditText) findViewById(R.id.etBaseAmount);
         seekbar = (SeekBar) findViewById(R.id.seekBar);
         tvTipAmount = (TextView) findViewById(R.id.tvTipAmount);
-        tvTotalAmount = (TextView) findViewById(R.id.tvTipAmount);
+        tvTotalAmount = (TextView) findViewById(R.id.tvTotalAmount);
 
         int initProgress = 15;
         seekbar.setProgress(initProgress);
         tvSeekbar.setText(initProgress + "%");
-        seekbar.setOnSeekBarChangeListener(this);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i(TAG, "onProgressChange: " + i);
+                tvSeekbar.setText(i + "%");
+                compute();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        etBaseAmount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Log.i(TAG, "Base: " + editable.toString());
+                compute();
+            }
+        });
     }
 
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-        Log.i(TAG, "onProgressChange: " + i);
-        tvSeekbar.setText(i + "%");
+    private void compute() {
+        if (etBaseAmount.getText().toString().length() == 0) {
+            return;
+        }
+
+        Float baseAmount = Float.parseFloat(etBaseAmount.getText().toString());
+        Integer tipPercent = seekbar.getProgress();
+        Float tipAmount = baseAmount * tipPercent / 100;
+        Float totalAmount = tipAmount + baseAmount;
+
+        tvTipAmount.setText(tipAmount + " $");
+        tvTotalAmount.setText(totalAmount + " $");
     }
 
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {}
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {}
 }
